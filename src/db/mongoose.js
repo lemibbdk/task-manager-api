a4ed28474f8e995e563e1f989326adc499a5df14
lemibbdk <milenkovicaleksa94@yahoo.com>
2021-01-26 15:38:01 +0100

    mongoose test

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,3 +6,7 @@ mongoose.connect(process.env.MONGODB_URL, {
     useCreateIndex: true,
     useFindAndModify: false
 }).then(r => console.log(r))
+    .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+});
